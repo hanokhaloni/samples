07d3ddbb6584f139e31784f1c0706292cf98e407
Hanokh.Aloni <Hanokh.Aloni@gmail.com>
2023-06-12 08:38:42 +0300

    Added kafka consumer and producer

---
+++ dotnet/kafka.pubsub/kafka.console/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace kafka.pubsub.console
+{
+    public class Message
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string? Data { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
--- dotnet/kafka.pubsub/kafka.console/Program.cs
+++ dotnet/kafka.pubsub/kafka.console/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace kafka.pubsub.console
+{
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var consumer = new Consumer<Message>();
+            var consumeTask = Task.Run(() => consumer.ConsumeAsync());
+
+
+            var producer = new Producer<Message>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                await producer.ProduceAsync(new Message
+                {
+                    Data = $"Pushing Data {i} !!",
+                });
+
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine("Publish Success!");
+
+        }
+    }
+}
