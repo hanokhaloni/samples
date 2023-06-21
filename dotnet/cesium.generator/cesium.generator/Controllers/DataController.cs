8de482fd691bbc43a9180f4b1f31553e33fd9864
Hanokh.Aloni <Hanokh.Aloni@gmail.com>
2023-06-21 22:09:32 +0300

    Added validations

--- dotnet/cesium.generator/cesium.generator/Controllers/DataController.cs
+++ dotnet/cesium.generator/cesium.generator/Controllers/DataController.cs
@@ -9,18 +9,38 @@ namespace cesium.generator.Controllers
     {
         [HttpGet]
         [Route("SplitLine")]
-        public JsonResult SplitLine(double x1, double y1, double x2, double y2)
+        public IActionResult SplitLine(double x1, double y1, double x2, double y2)
         {
+            if (!IsValidCoordinate(x1) || !IsValidCoordinate(y1) || !IsValidCoordinate(x2) || !IsValidCoordinate(y2))
+            {
+                return BadRequest("Invalid coordinates provided.");
+            }
+
             double[] linePoints = DataGenerator.SplitLine(x1, y1, x2, y2);
             return Json(linePoints);
         }
 
         [HttpGet]
         [Route("GenerateEllipse")]
-        public JsonResult GenerateEllipse(double x1, double y1, double x2, double y2, int amountOfPoints)
+        public IActionResult GenerateEllipse(double x1, double y1, double x2, double y2, int amountOfPoints)
         {
+            if (!IsValidCoordinate(x1) || !IsValidCoordinate(y1) || !IsValidCoordinate(x2) || !IsValidCoordinate(y2))
+            {
+                return BadRequest("Invalid coordinates provided.");
+            }
+
+            if (amountOfPoints <= 0)
+            {
+                return BadRequest("Amount of points must be a positive integer.");
+            }
+
             double[] ellipsePoints = DataGenerator.GenerateEllipse(x1, y1, x2, y2, amountOfPoints);
             return Json(ellipsePoints);
+        }
+
+        private bool IsValidCoordinate(double coordinate)
+        {
+            return coordinate<180.0f && coordinate>-180.0f; 
         }
     }
 }
